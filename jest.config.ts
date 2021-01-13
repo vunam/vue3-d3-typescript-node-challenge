27acc5d6f743f282dadb821ec6228b6cb98d9490
Vu Nam Nguyen <vu@strangelab.co.uk>
2021-01-13 20:57:08 +0100

    Add initial typescript setup and jest setup

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  collectCoverage: true,
+};
+
+export default config;
---
+++ src/migration/migration.test.ts
@@ -0,0 +1,6 @@
+
+describe('test', () => {
+  it('test', () => {
+    expect(true).toBe(true);
+  })
+});
