f75bd9448e7db0685602d786986faa660c85c19f
Vu Nam Nguyen <vu@strangelab.co.uk>
2021-01-13 21:20:21 +0100

    Add neo4j setup and initial migration script

--- Readme.md
+++ Readme.md
@@ -6,3 +6,10 @@
 - Intro
 - Instructions
 - Notes
+
+Requirements 
+Set variable `NEO4J_AUTH` in the `.env` to `neo4j/any-password-here`. Install neo4j or run the following docker command:
+
+```
+  docker run --name neo4j -p7474:7474 -p7687:7687 -d --env-file .env neo4j:latest
+```
--- src/migration/migration.ts
+++ src/migration/migration.ts
@@ -1,11 +1,22 @@
 import 'dotenv/config';
+import neo4j from 'neo4j-driver';
 
-export const up = (): void => {
-  console.log('hello', process.env.ENV_VAR_TEST);
+const [neo4jUser, neo4jPassword] = process.env.NEO4J_AUTH.split('/');
+const driver = neo4j.driver(process.env.NEO4J_DB_URL, neo4j.auth.basic(neo4jUser, neo4jPassword));
+
+export const up = async (): Promise<void> => {
+  const session = driver.session();
+
+  try {
+    const result = await session.run('SHOW DATABASES');
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+
+  await session.close();
 };
 
-export const down = (): void => {
-
-};
+export const down = (): void => {};
 
 up();
