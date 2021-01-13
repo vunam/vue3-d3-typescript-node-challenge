e35f2e9240453d2722985b8e3c01b9682f2e88e6
Vu Nam Nguyen <vu@strangelab.co.uk>
2021-01-13 22:01:10 +0100

    Add parent-child relationships to nodes

--- src/migration/migration.ts
+++ src/migration/migration.ts
@@ -1,49 +1,81 @@
 import 'dotenv/config';
+
+import chalk from 'chalk';
 import fetch from 'node-fetch';
+import PrettyError from 'pretty-error';
 
 import { initConnection } from './helpers/db';
+
+new PrettyError().start();
+
+const prettyLog = (text: string) => console.log(chalk.yellow(text));
 
 const migrationMode = process.argv[2];
 const driver = initConnection();
 
-const FETCH_OPTIONS = {
-  method: 'get',
-  headers: {'Content-Type': 'application/json'},
-};
-
 export const up = async (): Promise<void> => {
   const session = driver.session();
 
-	try {
-    const response = await fetch(process.env.DATA_FILE_URL, FETCH_OPTIONS);
+  try {
+    const response = await fetch(process.env.DATA_FILE_URL, {
+      method: 'get',
+      headers: { 'Content-Type': 'application/json' },
+    });
     const { data } = await response.json();
-    
+
     if (!data?.length) {
-      throw Error("No data has been received");
+      throw Error('No data has been received');
     }
 
     const query = ` 
       UNWIND $items as items
-      CREATE (a:Node) SET a = items
+      CREATE (node:Node) SET node = items
     `;
-  
-    const importData = {
-      items: data
-    };
-  
-		const result = await session.run(query, importData);
-		console.log(result);
-	} catch (err) {
-		console.error(err);
-	}
 
-	await session.close();
+    await session.run(query, {
+      items: data,
+    });
+
+    const queryRelations = `
+      match (a:Node)
+      where NOT (a.parent = "")
+      match (b:Node)
+      where b.name = a.parent
+      create (a)-[:CHILD_OF]->(b)
+    `;
+
+    await session.run(queryRelations);
+  } catch (err) {
+    console.error(err);
+  }
+
+  await session.close();
+  await driver.close();
 };
 
-export const down = (): void => {};
+export const down = async (): Promise<void> => {
+  const session = driver.session();
+  try {
+    const query = `
+      MATCH (n)
+      DETACH DELETE n
+    `;
 
-if (migrationMode === 'UP') {
-	up();
-} else {
-	down();
-}
+    await session.run(query);
+  } catch (err) {
+    console.error(err);
+  }
+
+  await session.close();
+  await driver.close();
+};
+
+const startProcess = async () => {
+  prettyLog(`-- Migration ${migrationMode} - started`);
+
+  await (migrationMode === 'UP' ? up() : down());
+
+  prettyLog('-- Process completed');
+};
+
+startProcess();
