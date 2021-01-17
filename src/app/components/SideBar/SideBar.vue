7c25ee11aa820a10f50817db4d77c46f3625038c
Vu Nam Nguyen <vu@strangelab.co.uk>
2021-01-17 18:07:31 +0100

    Add initial slide out to SideBar

--- src/app/components/SideBar/SideBar.vue
+++ src/app/components/SideBar/SideBar.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="SideBar-container">
-    SideBar
+  <div class="SideBar-container" :class="{ 'SideBar-container--active': sideBarOpen }">
+    <div class="SideBar-inner">
+      SideBar
+      <button @click="onClick">Toggle sidebar</button>
+      {{ sideBarOpen }}
+    </div>
   </div>
 </template>
 
@@ -8,12 +12,32 @@
   .SideBar-container {
     width: 300px;
     background: #eee;
+    transition: transform 0.5s;
+  }
+
+  .SideBar-inner {
     padding: 20px;
+  }
+
+  .SideBar-container--active {
+    transform: translate3d(-300px, 0, 0);
   }
 </style>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent, reactive } from "vue"
 
-export default defineComponent({})
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      sideBarOpen: false,
+    });
+    return state;
+  },
+  methods: {
+    onClick() {
+      this.sideBarOpen = !this.sideBarOpen;
+    }
+  }
+})
 </script>
